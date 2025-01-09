bbb28a9b260c84651f5d28c7cf77d4d7a36c9bdf
Артур Денцель <dencelq_2007@mail.ru>
2025-01-09 23:35:28 +0600

    1

---
+++ blogicum/pages/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+def rules(request):
+    return render(request, 'pages/rules.html')
+
+
+def csrf_failure(request, reason=''):
+    return render(request, 'pages/403csrf.html', status=403)
+
+
+def page_not_found(request, exception):
+    return render(request, 'pages/404.html', status=404)
+
+
+def server_error(request):
+    return render(request, 'pages/500.html', status=500)
